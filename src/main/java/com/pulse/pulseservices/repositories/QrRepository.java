a93619ad1cc7968acc92befc2a88d3d5d576ef9c
Dajve Echols <dajvelechols@gmail.com>
2024-05-20 00:06:06 -0500

    small clean up

--- src/main/java/com/pulse/pulseservices/repositories/QrRepository.java
+++ src/main/java/com/pulse/pulseservices/repositories/QrRepository.java
@@ -1,8 +1,7 @@
 package com.pulse.pulseservices.repositories;
 
 import com.pulse.pulseservices.entity.Qr;
-import com.pulse.pulseservices.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QrRepository  extends JpaRepository<Qr, Integer> {
+public interface QrRepository extends JpaRepository<Qr, Integer> {
 }
